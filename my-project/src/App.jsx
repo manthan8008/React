f9c08127efbb388ce649cd16005917ca91337470
manthan8008 <jainn.manthann@gmail.com>
2026-06-05 21:17:18 +0530

    commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import Navbar from "./Navbar";
 
@@ -10,7 +7,29 @@ function App() {
 
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <div className="bg-[url(./src/assets/hero3.jpg)] bg-fixed h-dvh w-full bg-cover text-3xl z-0 text-black justify-center flex flex-wrap px-5">
+        <div className="h-120 w-100 bg-white relative top-1/3 right-1/3 pt-5 text-5xl px-3 custom-font">
+          <div className="z-0 h-50 px-5">
+            <h1 className="text-4xl ">Elegance in every detail</h1>
+          </div>
+          <div className="px-5 ">
+            <p className="text-2xl">
+              Discover a world where tradition meets modernity in every
+              exquisite piece. Our artisan-made jewelry is a blend of heritage
+              craftsmanship and contemporary allure.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#2d3433] h-auto">
+        <div className="py-10 flex flex-wrap justify-center gap-10">
+          <div className="bg-white w-100 h-150 rounded-2xl"></div>
+          <div className="bg-white w-100 h-150 rounded-2xl"></div>
+          <div className="bg-white w-100 h-150 rounded-2xl"></div>
+          <div className="bg-white w-100 h-150 rounded-2xl"></div>
+        </div>
+      </div>
     </>
   );
 }
--- my-project/src/Navbar.jsx
+++ my-project/src/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { AnimatePresence, motion } from "framer-motion";
+import "./Navbar.css";
 
 function Navbar() {
   const FlyoutLink = ({ children, href, FlyoutContent }) => {
@@ -12,13 +13,13 @@ function Navbar() {
         onMouseLeave={() => setOpen(false)}
         className="relative h-fit w-fit"
       >
-        <a href={href} className="relative text-white">
+        <a href={href} className="relative text-black">
           {children}
           <span
             style={{
               transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
             }}
-            className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left rounded-full bg-indigo-300 transition-transform duration-300 ease-out"
+            className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left rounded-full bg-black transition-transform duration-300 ease-out"
           ></span>
         </a>
         <AnimatePresence>
@@ -27,9 +28,9 @@ function Navbar() {
               initial={{ opacity: 0, y: 5 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: 5 }}
-              className="absolute left-1/2 top-15 -translate-x-1/2 bg-white text-black"
+              className="absolute left-1/2 top-17 -translate-x-1/2 bg-white text-black"
             >
-              <div className="absolute -top-6 left-0 right-0 h-6" />
+              <div className="absolute -top-6 left-0 right-0 h-6 " />
               <div className="absolute left-1/2 top-0 rotate-45 bg-white h-4 w-4 -translate-x-1/2 -translate-y-1/2" />
               <FlyoutContent />
             </motion.div>
@@ -41,7 +42,7 @@ function Navbar() {
 
   const HelloContent = () => {
     return (
-      <div className="w-64 font-mono font-semibold bg-white p-6 shadow-xl text-2xl">
+      <div className="w-100 font-mono font-semibold bg-white p-6 shadow-xl text-2xl">
         <div>
           <a href="#" className="block  hover:underline">
             Neckpieces
@@ -59,11 +60,40 @@ function Navbar() {
 
   return (
     <>
-      <div className="flex h-screen justify-center bg-neutral-900 px-3 text-3xl py-12 font-mono font-bold">
-        <FlyoutLink href="#" FlyoutContent={HelloContent}>
-          Content
-        </FlyoutLink>
-      </div>
+      <header className="bg-[#fffef8] flex justify-center sticky top-0 z-10">
+        <nav className=" flex items-center justify-between bg-[#fffef8] px-5 text-2xl py-6 custom-font font gap-3 w-[92%] text-black">
+          <div>
+            CHAMHAQ
+          </div>
+          <div>
+            <ul className="flex gap-[4vw] items-center text-black">
+              <li>
+                <FlyoutLink href="#" FlyoutContent={HelloContent}>
+                  NECKPIECES
+                </FlyoutLink>
+              </li>
+              <li>
+                <FlyoutLink href="#" FlyoutContent={HelloContent}>
+                  BRACELETS
+                </FlyoutLink>
+              </li>
+              <li>
+                <FlyoutLink href="#" FlyoutContent={HelloContent}>
+                  EARRINGS
+                </FlyoutLink>
+              </li>
+              <li>
+                <FlyoutLink href="#" FlyoutContent={HelloContent}>
+                  RINGS
+                </FlyoutLink>
+              </li>
+              <li>
+                <FlyoutLink href="#">About</FlyoutLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </>
   );
 }
